21982dc2a94781903f8803c71367520c90b43e2a
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-01-25 12:01:10 -0500

    fix: #choose_type to return HotDog @type

--- models/hot_dog.rb
+++ models/hot_dog.rb
@@ -2,13 +2,13 @@ class HotDog
   attr_reader :type, :bun, :condiments
 
   def initialize
-    @hot_dog = nil
+    @type = nil
     @bun = nil
     @condiments = nil
   end
 
   $error_message = "\nWe don't offer that here. \nPlease enter a valid option.\n----------------------------\n"
-  $prompt = "-->"
+  $prompt = "--> "
 
   def choose_type
     puts "Inside HotDog#choose_type"
@@ -18,7 +18,7 @@ class HotDog
     dog_confirmation = "Thank you for choosing a hot dog type!\nNow let\'s figure out what kind of bun you want."
 
     # Asks the user a question
-    puts "\nWhat type of hotdog would you like?\n-----------------------------------"
+    puts "\nWhat type of hot dog would you like?\n-----------------------------------"
 
     # prints out each item of the array to the command line
     dog_type.each_with_index do |dog, index|
@@ -26,25 +26,21 @@ class HotDog
     end
 
     # ask the user for input and turn input into an integer
-    print prompt
-    user_input_dog = $stdin.gets.chomp.to_i
+    print $prompt
+    @type = $stdin.gets.chomp.to_i
 
     # checks to see whether we can move onto choosing a bun
-    if user_input_dog.between?(1, 4)
+    if @type.between?(1, 4)
       puts dog_confirmation
-      # will start the choose_bun method
-      choose_bun
     else
-      while user_input_dog > 4
-        puts error_message
-        print prompt
-        user_input_dog = gets.chomp.to_i
+      while @type > 4
+        puts $error_message
+        print $prompt
+        @type = gets.chomp.to_i
       end
-      puts dog_confirmation
-      # will start the choose_bun method
-      choose_bun
     end
-    order << "#{dog}"
+
+    @type
   end
 
   def choose_bun
