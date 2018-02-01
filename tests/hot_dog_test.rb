f8e2915e2e172c476f848311d891b0397ceb1afb
Yosevu Kilonzo <yosevu@heysparkbox.com>
2018-02-01 09:11:16 -0500

    refactor: Update HotDog tests

--- tests/hot_dog_test.rb
+++ tests/hot_dog_test.rb
@@ -4,33 +4,30 @@ require_relative '../models/hot_dog'
 class HotDogTest < Minitest::Test
   def setup
     @hot_dog = HotDog.new
-    @index_1 = 1
-    @index_2 = 2
+    @meat = "beef"
+    @bun = "whole wheat"
+    @condiment_1 = "ketchup"
+    @condiment_2 = "mustard" 
   end
   
   # choose_hot_dog tests
   
   # choose_bun tests
   def test_choose_bun_returns_a_string
-    assert_kind_of(String, @hot_dog.choose_bun(@index_1))
+    assert_kind_of(String, @hot_dog.meat = @meat)
   end
   
   def test_choose_bun_returns_selected_bun
-    assert_equal(@hot_dog.choose_bun(@index_1), "whole wheat")
+    assert_equal(@hot_dog.bun = @bun, "whole wheat")
   end
   
   # choose_condiment test
   def test_returns_1_selected_condiment
-    @hot_dog.choose_condiment(@index_1)
-    assert_equal(@hot_dog.condiments, ["ketchup"])
+    assert_equal(@hot_dog.condiments << "ketchup", ["ketchup"])
   end
   
   def test_returns_2_selected_condiments
-    # adds "ketchup" to @condiments
-    @hot_dog.choose_condiment(@index_1)
-    # adds "relish" to @condiments
-    @hot_dog.choose_condiment(@index_2)
-
-    assert_equal(@hot_dog.condiments, ["ketchup", "mustard"])
+    assert_equal(@hot_dog.condiments << "ketchup", ["ketchup"])
+    assert_equal(@hot_dog.condiments << "mustard", ["ketchup", "mustard"])
   end
 end
